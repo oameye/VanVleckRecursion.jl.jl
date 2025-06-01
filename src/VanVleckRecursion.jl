1b64f4d8237fffc0581dd9572071441d398047ba
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-02 00:16:31 +0200

    export Terms and Term in VanVleckRecursion module

--- src/VanVleckRecursion.jl
+++ src/VanVleckRecursion.jl
@@ -8,6 +8,7 @@ using Printf, LaTeXStrings
 
 # Export Kamiltonian and Generator functions
 export K, S, kamiltonian_get, generator_get, set_hamiltonian!, clear_caches!, latex
+export Terms, Term
 
 include("types.jl")
 include("operations.jl")
