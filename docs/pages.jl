99525cc43245388a546bc5cb1080bcd13811fcab
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-02 17:42:40 +0200

    format

--- docs/pages.jl
+++ docs/pages.jl
@@ -2,6 +2,5 @@ pages = [
     "Home" => "index.md",
     "API" => "API.md",
     "Literature" => "literature.md",
-    "Examples" =>
-        ["van Vleck expansion" => "examples/van_Vleck_expansion_method.md"],
+    "Examples" => ["van Vleck expansion" => "examples/van_Vleck_expansion_method.md"],
 ]
