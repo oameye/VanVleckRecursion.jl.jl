fb448b8b6bd4cf69b0d6bb7bee6ceb4494173161
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-08 17:36:55 +0200

    test: add DispatchDoctor (no type-stability)

--- src/VanVleckRecursion.jl
+++ src/VanVleckRecursion.jl
@@ -2,20 +2,21 @@
 $(DocStringExtensions.README)
 """
 module VanVleckRecursion
+using DispatchDoctor: @stable
+@stable default_mode = "disable" begin
+    using DocStringExtensions: DocStringExtensions
+    using LaTeXStrings: LaTeXStrings
 
-using DocStringExtensions: DocStringExtensions
-using LaTeXStrings: LaTeXStrings
+    # Export Kamiltonian and Generator functions
+    export K, S, kamiltonian_get, generator_get, set_hamiltonian!, clear_caches!, latex
+    export Terms, Term
 
-# Export Kamiltonian and Generator functions
-export K, S, kamiltonian_get, generator_get, set_hamiltonian!, clear_caches!, latex
-export Terms, Term
-
-include("types.jl")
-include("operations.jl")
-include("comparison.jl")
-include("collections.jl")
-include("arithmetic.jl")
-include("display.jl")
-include("kamiltonian.jl")
-
+    include("types.jl")
+    include("operations.jl")
+    include("comparison.jl")
+    include("collections.jl")
+    include("arithmetic.jl")
+    include("display.jl")
+    include("kamiltonian.jl")
+end
 end # module VanVleckRecursion
